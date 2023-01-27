81b5003e7a5371929e744d01f1a8dd11cbd5ae06
Junaid Ahmed <74968802+JunaidAhmed032@users.noreply.github.com>
2023-01-27 09:10:58 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import matplotlib.pylab as plt
+import pandas as pd
+#
+# df = pd.read_csv('weatherAUS.csv')
+# df = pd.DataFrame(df)
+# melb = df[df['Location'] == "Melbourne"]
+# melb = pd.DataFrame(melb)
+# melb['Date'] = pd.to_datetime(melb['Date'])
+# plt.plot(melb['Date'], melb['Temp3pm'])
+# plt.show()
+
